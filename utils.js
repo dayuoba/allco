a39699eb888c6c8057714127383c69eafe12d7ec
yuyang <yuyang@goyoo.com>
2017-03-19 22:02:32 +0800

    abstract the wrapper functions,now we noly need to enum the modules to be exported

--- utils.js
+++ utils.js
@@ -1,2 +1,34 @@
 const utils = module.exports = {};
 
+utils.wrap2AllcoFns = function(nativeModule) {
+	return async function() {
+		let err, rep;
+		try {
+			data = await promisefulNativeModule(nativeModule).apply(null, convertArgs2Array(arguments));
+		} catch(e) {
+			err = e;
+		}
+
+		return [err, data];
+	}
+}
+
+promisefulNativeModule = function(nativeModule) {
+	return async function() {
+		let args = convertArgs2Array(arguments);
+		return new Promise((resolve, reject) => {
+			nativeModule.apply(null, args.concat(wrapperedCallback(resolve, reject)))
+		});
+	}
+}
+
+convertArgs2Array = function(args = null) {
+	return [].slice.call(args) || [];
+}
+
+wrapperedCallback = function(resolve, reject) {
+	return function(err, rep) {
+		if (err) return reject(err);
+		resolve(rep);
+	}
+}
